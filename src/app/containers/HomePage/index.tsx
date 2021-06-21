b7d8a47b12cb56c1b44633d640e27a2c7c33be2b
tonystanktinio <timmybrayantbente4@gmail.com>
2021-06-21 14:09:03 +0800

    first commit

---
+++ src/app/containers/HomePage/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "styled-components";
+import { Navbar } from "../../components/navbar";
+import { TopSection } from "./topSection";
+import { BookCard } from "../../components/bookCard";
+import { Marginer } from "../../components/marginer";
+import { BookingSteps } from "./bookingSteps";
+import { AboutUs } from "./aboutUs";
+import { TopCars } from "./topCars";
+import { Footer } from "../../components/footer";
+
+import tw from "twin.macro";
+
+const PageContainer = styled.div`
+  ${tw`
+    flex
+    flex-col
+    w-full
+    h-full
+    items-center
+    overflow-x-hidden
+  `}
+`;
+
+export function HomePage() {
+  return (
+    <PageContainer>
+      <Navbar />
+      <TopSection />
+      <Marginer direction="vertical" margin="4em" />
+      <BookCard />
+      <Marginer direction="vertical" margin="10em" />
+      <BookingSteps />
+      <Marginer direction="vertical" margin="8em" />
+      <AboutUs />
+      <Marginer direction="vertical" margin="8em" />
+      <TopCars />
+      <Footer /> 
+    </PageContainer>
+  );
+}
